16fdcfa20e68a23e5abd4c6cafd49f41271c3ade
OlmanAQ <61434202+OlmanAQ@users.noreply.github.com>
2021-06-16 16:41:57 -0600

    inicio

--- main.cpp
+++ main.cpp
@@ -2,8 +2,61 @@
 
 using namespace std;
 
+void login(){
+    int opcion;
+
+    bool repetir = true;
+
+    do
+    {
+        system("cls");
+
+        // Texto del menú que se verá cada vez
+        cout << "\n\n\t\t\tChatbot" << endl;
+        cout << "\t\t\t----------------" << endl;
+        cout << "\n Hola usuario. Soy el Chatbot. ¿Dime que quieres que hacer? " << endl;
+        cout << "\n\n\t\t\tEstos son mis comandos" << endl;
+        cout << "\tRealizar nuevo carrito de compras" << endl;
+        cout << "\tModificar el carrito de compras" << endl;
+        cout << "\tCancelar un carrito de compras" << endl;
+        cout << "\tConsultar un carrito de compras" << endl;
+
+        cout << "\n\tIngrese una opcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            //string admin;
+            //admin=elegirAdmin()
+            menuAdmin();
+            break;
+
+        case 2:
+            //string profe;
+            //elegirProfe();
+            menuProfe();
+            break;
+
+        case 3:
+            //string estudiante;
+            //elegirEstudiante();
+            menuEstudiante();
+            break;
+
+        case 0:
+            repetir = false;
+            break;
+        }
+    } while (repetir);
+
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //cargardatos();
+    login();
+
     return 0;
 }
